7830e79086c254ba75f5336cd40e5789176d5999
LLi <lli@ims.ch>
2017-11-14 08:55:20 +0100

    Developper Key und neue Klasse

---
+++ app/src/main/java/ch/playtube/controller/DeveloperKey.java
@@ -0,0 +1,10 @@
+package ch.playtube.controller;
+
+/**
+ * Created by LLi on 14.11.2017.
+ */
+
+public class DeveloperKey {
+
+    public static final String DEVELOPPER_KEY = "AIzaSyCy24NuwmEScFFcKQv7rx-8GvN7KGwHcQI";
+}
---
+++ app/src/main/java/ch/playtube/controller/SearchController.java
@@ -0,0 +1,10 @@
+package ch.playtube.controller;
+
+/**
+ * Created by LLi on 14.11.2017.
+ */
+
+public class SearchController {
+
+    <
+}
