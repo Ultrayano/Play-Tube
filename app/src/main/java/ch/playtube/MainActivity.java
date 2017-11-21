93cf09dba680b643c7ebab0edd7caadddb6971b3
ue70629 <david.haenni@sbb.ch>
2017-11-21 15:37:08 +0100

    Design changes

--- app/src/main/java/ch/playtube/MainActivity.java
+++ app/src/main/java/ch/playtube/MainActivity.java
@@ -1,15 +1,12 @@
 package ch.playtube;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
